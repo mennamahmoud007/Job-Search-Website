4c7e004999fbd31e397589d592efd2b5a5ba5873
Rahma <rahmaymi24@gmail.com>
2026-04-01 11:20:46 +0200

    Fixed GuestBrowse & Filters

--- JavaScript/browsescript.js
+++ JavaScript/browsescript.js
@@ -1,116 +1,30 @@
 // ============================================
 //              Browse Page
 // ============================================
-/*const jobs = [ //ARRAY OF OBJECTS, EACH OBJECT HAS KEYS
-    {
-        title: "Frontend Developer",
-        company: "TechCorp - Cairo, Egypt",
-        salary: "$2,500/month",
-        experience: "2-4",
-        schedule: "full-time",
-        location: "onsite",
-        status: "Open",
-        link: "job-details.html"
-    },
-    {
-        title: "Data Analyst",
-        company: "Insight Co - Remote",
-        salary: "$1,800/month",
-        experience: "0-1",
-        schedule: "part-time",
-        location: "remote",
-        status: "Open",
-        link: "job-details.html"
-    },
-    {
-        title: "UI/UX Designer",
-        company: "Creative Studio - Alexandria, Egypt",
-        salary: "$2,000/month",
-        experience: "2-4",
-        schedule: "full-time",
-        location: "onsite",
-        status: "Closed",
-        link: "job-details.html"
-    },
-    {
-        title: "Backend Developer",
-        company: "NexaSoft - Cairo, Egypt",
-        salary: "$3,000/month",
-        experience: "+4",
-        schedule: "full-time",
-        location: "onsite",
-        status: "Open",
-        link: "job-details.html"
-    },
-    {
-        title: "Mobile App Developer",
-        company: "AppWorks - Alexandria, Egypt",
-        salary: "$2,200/month",
-        experience: "2-4",
-        schedule: "full-time",
-        location: "onsite",
-        status: "Closed",
-        link: "job-details.html"
-    },
-    {
-        title: "DevOps Engineer",
-        company: "CloudBase - Remote",
-        salary: "$3,500/month",
-        experience: "+4",
-        schedule: "full-time",
-        location: "remote",
-        status: "Open",
-        link: "job-details.html"
-    }
-];*/ 
-//Create HTML cards dynamically
-// function buildCards(jobsArray){
-
-//     const jobList = document.querySelector(".job-list");
-
-//     jobList.innerHTML=  "";
-
-//     jobsArray.forEach(function(job) {
-//         const li =document.createElement("li");
-//         const article = document.createElement("article");
-
-//         article.dataset.schedule = job.schedule;
-//         article.dataset.location = job.location;
-//         article.dataset.experience = job.experience;
-//         //backticks are template literals, allows multiple lines and injection of variables
-//         article.innerHTML = ` 
-//             <h3>${job.title}</h3>
-//             <p>${job.company}</p>
-//             <p>Salary: ${job.salary}</p>
-//             <p>Experience: ${job.experience}</p>
-//             <p>Schedule: ${job.schedule}</p>
-//             <span >Status: ${job.status}</span>
-//             <a href="${job.link}">View Job Details</a>
-//         `;
-
-//         li.appendChild(article);
-//         jobList.appendChild(li);
-//     });
-// }
+const isGuest = window.location.href.includes("guestBrowse");
 const jobListTag = document.querySelector('.job-list');
 const allJobs = JSON.parse(localStorage.getItem("jobs")) || [];
 
 function displayJobs(jobsToRender) {
-    jobListTag.innerHTML = ""; 
+    let html = "";
     jobsToRender.forEach(job => {
-        jobListTag.innerHTML += `
+        html += `
+        <li>
             <article>
                 <h3>${job.title}</h3>
                 <p>${job.company} - ${job.location}</p> 
                 <p>Salary: ${job.salary}</p>
                 <p>Experience: ${job.experience}</p>
                 <p>Schedule: ${job.schedule}</p>
-                <span >Status: ${job.status}</span>
-                <a href="job-details.html?id=${job.id}">View Details</a>
+                <span style="color: ${job.status === 'Open' ? 'green' : 'brown'}">Status: ${job.status}</span>
+                <a href="${isGuest ? 'login.html' : 'job-details.html?id=' + job.id}">View Details</a>
             </article>
-        `;
+        </li>`;
     });
+    jobListTag.innerHTML = html;
 }
+ 
+
 
 /*______________________________________________________________________ */
 // WHEN any checkbox changes, run this function
@@ -127,7 +41,7 @@ function filterjobs(){
                 .map(cb => cb.value);
                 const searchvalue = document.querySelector('#job-search').value.toLowerCase();  
                 //filter each job based on all filters
-                const filteredjobs = jobs.filter(function(job){
+                const filteredjobs = allJobs.filter(function(job){
                     if(scheduleValues.length > 0 && !scheduleValues.includes(job.schedule))
                         return false;
                     if(locationValues.length > 0 && !locationValues.includes(job.location))
